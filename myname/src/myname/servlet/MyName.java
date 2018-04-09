7fd66d5f30a058dd862e3ef659462ea6c87f68e1
Sundari Deepan <sudeep@Sundaris-MacBook-Air.local>
2018-04-09 10:25:57 +0530

    myname

---
+++ myname/src/myname/servlet/MyName.java
@@ -0,0 +1,46 @@
+package myname.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyName
+ */
+@WebServlet("/MyName")
+public class MyName extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyName() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.setContentType("text/html");
+        PrintWriter printWriter  = response.getWriter();
+        printWriter.println("<h1>Sundari</h1>");
+        printWriter.println("<h1>Deepan</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
